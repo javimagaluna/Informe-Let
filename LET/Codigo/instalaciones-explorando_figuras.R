c4b9f78e8029e45fdb6ab8758718f1ca3f118e87
javierapreuss <javiera.preuss@uc.cl>
2020-12-14 23:04:00 -0300

    Figuras y codigos que faltaban por subir

--- LET/Codigo/instalaciones-explorando_figuras.R
+++ LET/Codigo/instalaciones-explorando_figuras.R
@@ -51,9 +51,9 @@ instalaciones_cambio %>%
   ggplot(., aes(x=pais, y=Tasa,fill= Establecimiento)) + theme_minimal()+ geom_bar(stat="identity", position=position_dodge())
 
 # comparando --------------------------------------------------------------
-max_inst<- instalaciones_SU%>%filter(instalaciones_SU$hospitales_psiquiatricos>1.9 | instalaciones_SU$unidades_de_salud>4)
+max_inst<- instalaciones_SU%>%filter(instalaciones_SU$hospitales_psiquiatricos>4 | instalaciones_SU$unidades_de_salud>4)
 
-ggplot(instalaciones_SU, aes(hospitales_psiquiatricos, unidades_de_salud))+geom_point(alpha=0.5, col="purple4")+ theme_minimal()+ labs( y="Unidades de salud en hospitales generales", x= "Hospitales psiquiatricos", title="Relación entre Hospitales psiquiatricos y unidades de salud mental en hospitales generales en 2016 a nivel mundial")+ ggrepel::geom_label_repel(aes(label = Pais), data = max_inst)
+ggplot(instalaciones_SU, aes(hospitales_psiquiatricos, unidades_de_salud))+geom_point(alpha=0.5, col="purple4")+ theme_minimal()+ labs( y="Unidades de salud en hospitales generales", x= "Hospitales psiquiatricos", title="Relación entre unidades de salud mental de hospitales generales y hospitales psiquiátricos en 2016 a nivel mundial")+ ggrepel::geom_label_repel(aes(label = Pais), data = max_inst)
 
 ggplot(instalaciones_SU, aes(hospitales_psiquiatricos, unidades_de_salud))+geom_point(alpha=0.5, col="purple4")+ theme_minimal()+ labs( y="Unidades de salud en hospitales generales", x= "Hospitales psiquiatricos", title="Relación entre Hospitales psiquiatricos y unidades de salud mental en hospitales generales en 2016 a nivel mundial")+ ylim(c(0,1.2))+ xlim(c(0,1))+ geom_abline(intercept=0, slope=1)
 
